cee2dbc473bf0f2d62abca3e04cd8e1989054532
aansh919821 <139904463+aansh919821@users.noreply.github.com>
2024-05-26 08:10:23 +0530

    Update Leetcode1.cpp

--- Assignment1 Project ACA/Leetcode1.cpp
+++ Assignment1 Project ACA/Leetcode1.cpp
@@ -1,10 +1,29 @@
 class Solution {
-public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int m = nums[0], n = nums.size();
-        for(int i = 1;i<nums.size();i++){
-            m = m ^ nums[i];
+    public int singleNonDuplicate(int[] nums) {
+        if(nums.length==1){
+            return nums[0];
         }
-        return m;
+        int i=0;
+        int j=nums.length-1;
+        while(i<j){
+            int mid=(i+j)/2;
+            if(mid%2==0){
+                if(nums[mid]==nums[mid+1]){
+                    i=mid;
+                }
+                else{
+                    j=mid;
+                }
+            }
+            else{
+                if(nums[mid-1]==nums[mid]){
+                    i=mid+1;
+                }
+                else{
+                    j=mid-1;
+                }
+            }
+        }
+        return nums[i];
     }
-};
+}
